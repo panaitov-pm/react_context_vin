6f9011f94d9fb95acff28ac23c9977ed77382978
Pavel <p.panaitov@travellizy.com>
2020-01-20 14:19:34 +0200

    Updates

---
+++ precache-manifest.be3804c8602f827e766e2c42857826d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5580ee2b7a7b55335360587ee48392",
+    "url": "/react_context_vin/index.html"
+  },
+  {
+    "revision": "2093a9fea6081dd7c629",
+    "url": "/react_context_vin/static/css/main.b84e1194.chunk.css"
+  },
+  {
+    "revision": "1d3b32efbcddb7fd2cc2",
+    "url": "/react_context_vin/static/js/2.aa68cfc5.chunk.js"
+  },
+  {
+    "revision": "3a045352065895fd8ed6e32dffbaec19",
+    "url": "/react_context_vin/static/js/2.aa68cfc5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2093a9fea6081dd7c629",
+    "url": "/react_context_vin/static/js/main.f920b21b.chunk.js"
+  },
+  {
+    "revision": "4581024cd7857233ad74",
+    "url": "/react_context_vin/static/js/runtime-main.98f8368f.js"
+  }
+]);
